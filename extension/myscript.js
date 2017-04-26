e992804980dc588372d953e177840e7f0b37e46e
toni <unmail.toni@gmail.com>
2017-04-26 17:05:15 +0200

    v0.1 extension done

---
+++ extension/myscript.js
@@ -0,0 +1,7 @@
+console.log('LOOOOOOOOOOOOl')
+var body = document.querySelector('body')
+var div = document.createElement('div')
+div.setAttribute('id','main')
+body.innerHTML = ''
+body.appendChild(div)
+console.log(body)
